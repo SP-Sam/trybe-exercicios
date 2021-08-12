3a08a4a56f9c07f464824431d8927b325b267f7f
Samuel Pereira <gmr.quest.10@gmail.com>
2021-08-12 11:44:32 -0300

    Resolucao do exercicio 5

--- fundamentos/bloco-4-javascript-logica/4.4/exercicios.js
+++ fundamentos/bloco-4-javascript-logica/4.4/exercicios.js
@@ -104,8 +104,26 @@ function largestName(arr) {
 
 console.log(largestName(nomes)); */
 
-/* 
-  EXERCÍCIO 5 
+/* Exercício 5 (Entendido e resolvido com a ajuda do gabarito)
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero += 1;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+  return numeros[indexNumeroRepetido];
+}
 */
 
 /* Exercício 6
