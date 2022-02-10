94d644b4da972748798c07ebfae241cbb3d7bf3e
Samuel Pereira <gmr.quest.10@gmail.com>
2022-02-10 09:43:26 -0300

    Exercicios 1 e 2 concluidos

---
+++ 3.back-end/bloco-22-introducao-ao-node/22.2/ex01/ex01.js
@@ -0,0 +1,28 @@
+// Exercício 1
+const calculate = (numA, numB, numC) => {
+  const promise = new Promise((resolve, reject) => {
+    if (
+      typeof numA != 'number' ||
+      typeof numB != 'number' ||
+      typeof numC != 'number'
+    ) reject(new Error('Informe apenas numeros'));
+
+    const result = (numA + numB) * numC;
+
+    if (result < 50) reject(new Error('Valor muito baixo'));
+    
+    resolve(`(${numA} + ${numB}) x ${numC} = ${result}`);
+  });
+
+  return promise;
+};
+
+// Exercício 2
+const randomA = Math.floor(Math.random() * 100 + 1);
+const randomB = Math.floor(Math.random() * 100 + 1);
+const randomC = Math.floor(Math.random() * 100 + 1);
+
+calculate(randomA, randomB, randomC)
+  .then(result => console.log(`Calculo efetuado: ${result}`))
+  .catch(err => console.log(`ERRO: ${err.message}`));
+
