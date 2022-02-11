ea86866ed3f359d6073b20fcb75f600f3033cbde
Samuel Pereira <gmr.quest.10@gmail.com>
2022-02-11 20:42:12 -0300

    Adiciona exercicios de fixacao 2

--- 3.back-end/bloco-22-introducao-ao-node/22.4/hello-express/index.js
+++ 3.back-end/bloco-22-introducao-ao-node/22.4/hello-express/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
-
-app.use(cors());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
@@ -11,10 +8,35 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
-app.get('/recipes', (req, res) => {
-  res.json(recipes);
+app.get('/recipes/search', (req, res) => {
+  const { name, maxPrice, minPrice } = req.query;
+  
+  const filteredRecipes = recipes.filter(recipe => {
+    return (
+      recipe.name.includes(name) &&
+      recipe.price < parseInt(maxPrice) &&
+      recipe.price >= parseInt(minPrice)
+    );
+  });
+  
+  return res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  const recipe = recipes.find(recipe => recipe.id === parseInt(id));
+
+  if (!recipe) {
+    return res.status(404).json({ message: 'Recipe not found.' });
+  }
+  
+  return res.status(200).json(recipe);
+});
+
+app.get('/recipes', (_req, res) => {
+  return res.status(200).json(recipes);
 });
 
 app.listen(3001, () => {  
-  console.log('Aplicacao ouvindo na porta 3001');
+  console.log('Servidor rodando na porta 3001');
 });
