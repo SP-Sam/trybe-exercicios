4555b1f5aff6f553eb76eec4100be533529976d8
Samuel Pereira <gmr.quest.10@gmail.com>
2022-02-11 18:51:14 -0300

    Exercicio de fixacao 1

---
+++ 3.back-end/bloco-22-introducao-ao-node/22.4/ex01/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Agua Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/drinks', (_req, res) => {
+  return res.json(drinks);
+});
+
+app.get('/drinks/sorted', (_req, res) => {
+  const sortedDrinks = drinks.sort((a, b) => {
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
+    return 0;
+  });
+
+  return res.json(sortedDrinks);
+});
+
+app.listen(3001, () => {
+  console.log('Server rondando na porta 3001');
+});
