b4daa54eadbad13881e5e84727ab615d151bec01
Samuel Pereira <gmr.quest.10@gmail.com>
2022-02-15 12:09:25 -0300

    Conteudo do dia 22.4 finalizado

--- 3.back-end/bloco-22-introducao-ao-node/22.4/ex01/index.js
+++ 3.back-end/bloco-22-introducao-ao-node/22.4/ex01/index.js
@@ -1,7 +1,9 @@
-const { application } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 const drinks = [
   { id: 1, name: 'Refrigerante Lata', price: 5.0 },
@@ -44,6 +46,47 @@ app.get('/drinks/:id', (req, res) => {
 
 app.get('/drinks', (_req, res) => {
   return res.status(200).json(drinks);
+});
+
+app.post('/drinks', (req, res) => {
+  const { id, name, price } = req.body;
+
+  drinks.push({ id, name, price });
+
+  res.status(200).json({ message: 'New drink created!' });
+});
+
+app.put('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  const drinkIndex = drinks.findIndex(drink => drink.id === parseInt(id));
+
+  if (drinkIndex === -1) {
+    return res.status(404).json({ message: 'Drink not found!' });
+  }
+
+  drinks[drinkIndex] = { ...drinks[drinkIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+
+  const drinkIndex = drinks.findIndex(drink => drink.id === parseInt(id));
+
+  if (drinkIndex === -1) {
+    return res.status(404).json({ message: 'Drink not found!' });
+  }
+
+  drinks.splice(drinkIndex, 1);
+
+  res.status(204).end();
+});
+
+app.all('*', (req, res) => {
+  return res.status(404).json({ message: `Rota ${req.path} não existe!` });
 });
 
 app.listen(3002, () => {
--- 3.back-end/bloco-22-introducao-ao-node/22.4/hello-express/index.js
+++ 3.back-end/bloco-22-introducao-ao-node/22.4/hello-express/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
@@ -8,16 +10,28 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+app.get('/validateToken', (req, res) => {
+  const token = req.headers.authorization;
+
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Invalid Token!' });
+  }
+
+  res.status(200).json({ message: 'Valid Token!' });
+});
+
+app.get('/recipes', (_req, res) => {
+  return res.status(200).json(recipes);
+});
+
 app.get('/recipes/search', (req, res) => {
   const { name, maxPrice, minPrice } = req.query;
   
-  const filteredRecipes = recipes.filter(recipe => {
-    return (
-      recipe.name.includes(name) &&
-      recipe.price < parseInt(maxPrice) &&
-      recipe.price >= parseInt(minPrice)
-    );
-  });
+  const filteredRecipes = recipes.filter(recipe => (
+    recipe.name.includes(name) &&
+    recipe.price < parseInt(maxPrice) &&
+    recipe.price >= parseInt(minPrice)
+  ));
   
   return res.status(200).json(filteredRecipes);
 });
@@ -33,10 +47,49 @@ app.get('/recipes/:id', (req, res) => {
   return res.status(200).json(recipe);
 });
 
-app.get('/recipes', (_req, res) => {
-  return res.status(200).json(recipes);
+
+app.post('/recipes', (req, res) => {
+  const { id, name, price } = req.body;
+
+  recipes.push({ id, name, price });
+
+  res.status(201).json({ message: 'Recipe created successfully!' });
+});
+
+app.put('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  const recipeIndex = recipes.findIndex(recipe => recipe.id === parseInt(id));
+
+  console.log(recipeIndex);
+  if (recipeIndex === -1) {
+    return res.status(404).json({ message: 'Recipe not found!' });
+  }
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  
+  const recipeIndex = recipes.findIndex(recipe => recipe.id === parseInt(id));
+
+  if (recipeIndex === -1) {
+    return res.status(404).json({ message: 'Recipe not found!' });
+  }
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
+app.all('*', (req, res) => {
+  return res.status(404).json({ message: `Route ${req.path} not found!` });
 });
 
 app.listen(3001, () => {  
-  console.log('Servidor rodando na porta 3001');
+  console.log('Server running on 3001 port');
 });
