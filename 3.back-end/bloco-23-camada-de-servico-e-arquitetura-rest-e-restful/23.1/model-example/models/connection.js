a15e13938f7f7aadcd577c6631f7523a368dcd21
Samuel Pereira <72403810+SP-Sam@users.noreply.github.com>
2022-02-19 18:48:06 -0300

    Adiciona variável de ambiente à senha do MySQL

--- 3.back-end/bloco-23-camada-de-servico-e-arquitetura-rest-e-restful/23.1/model-example/models/connection.js
+++ 3.back-end/bloco-23-camada-de-servico-e-arquitetura-rest-e-restful/23.1/model-example/models/connection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'samuel',
-  password: 'spsamuel',
+  password: process.env.MYSQLPASSWORD,
   database: 'model_example',
   port: 3306,
 });
